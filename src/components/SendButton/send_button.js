f2b67e0b81f09e951cd0d7fb4da4dabc41e44661
Atirek Pothiwala <atirekpothiwala@gmail.com>
2025-02-25 01:38:34 +0530

    Send button fixed

--- src/components/SendButton/send_button.js
+++ src/components/SendButton/send_button.js
@@ -4,17 +4,24 @@ import imgSend from "../../assets/images/mail.png";
 
 const SendButton = ({ name, subject, message }) => {
   const sendMail = () => {
+    const encodedName = encodeURIComponent(name);
+    const encodedSubject = encodeURIComponent(subject);
+    const encodedMessage = encodeURIComponent(message);
+
     // Create a mailto link with user input
-    const mailtoLink = `mailto:atirekpothiwala@gmail.com?subject=${subject}&body=${message}%0A%0A-Regards,%0A${name}`;
+    const mailtoLink = `mailto:atirekpothiwala@gmail.com?subject=${encodedSubject}&body=${encodedMessage}%0A%0A-Regards,%0A${encodedName}`;
     // Open the user's email client
     window.location.href = mailtoLink;
     return mailtoLink;
   };
   return (
-    <button className="send-button" onClick={sendMail}>
-      <img src={imgSend} alt="Send" />
-      Send
-    </button>
+    <div className="send-button-warn">
+      <button className="send-button" onClick={sendMail}>
+        <img src={imgSend} alt="Send" />
+        Send
+      </button>
+      *Make sure you have an email app installed.
+    </div>
   );
 };
 
